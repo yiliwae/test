a8054f1dcd841666bea2d990faf10d1c954001a6
yiliwae <yi.li@wae.com>
2024-06-05 12:49:18 +0100

    create func1 on main

---
+++ code/fun1_add.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = fun1_add(inputArg1,inputArg2)
+%FUN1_ADD Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1+inputArg2;
+outputArg2 = inputArg2;
+end
+
