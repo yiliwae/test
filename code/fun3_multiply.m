15579c097de7b77824122bcbbe6ee447b89abf8f
yiliwae <yi.li@wae.com>
2024-06-05 12:58:30 +0100

    add func3 on branch 1

---
+++ code/fun3_multiply.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = fun3_multiply(inputArg1,inputArg2)
+%FUN3_MULTIPLY Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1*2;
+outputArg2 = inputArg2;
+end
+
